50d401e77cca7951978b3220369592d5d36ecfea
Anastasiya <v.anastasiya26@mail.ru>
2020-07-14 10:14:12 +0700

    form

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,19 @@
+const validateEmail = (email) => {
+  const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+  if (reg.test(email) === false) {
+    return false;
+  }
+  return true;
+};
+
+const validatePhone = (phone) => {
+  const reg = /^\d{10}$/;
+  if (reg.test(phone) === false) {
+    return false;
+  }
+  return true;
+};
+
 const menu = () => {
   const pageHeader = document.querySelector(`.page-header__wrapper`);
   const mainNav = document.querySelector(`.main-nav`);
@@ -34,11 +50,19 @@ const modalHandlers = () => {
   const inputPhone = modal.querySelector(`.modal__input--phone`);
   const inputEmail = modal.querySelector(`.modal__input--email`);
 
-  const validateEmail = () => {
-    const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-    const email = inputEmail.value;
+  if (localStorage.getItem(`phone`)) {
+    inputPhone.value = localStorage.getItem(`phone`);
+  }
 
-    if (reg.test(email) === false) {
+  if (localStorage.getItem(`email`)) {
+    inputEmail.value = localStorage.getItem(`email`);
+  }
+
+  const checkEmail = () => {
+    const email = inputEmail.value;
+    localStorage.setItem(`email`, email);
+
+    if (validateEmail(email) === false) {
       inputEmail.parentNode.classList.add(`modal__error`);
       return false;
     } else {
@@ -47,11 +71,11 @@ const modalHandlers = () => {
     }
   };
 
-  const validatePhone = () => {
-    const reg = /^\d{10}$/;
+  const checkPhone = () => {
     const phone = inputPhone.value;
+    localStorage.setItem(`phone`, phone);
 
-    if (reg.test(phone) === false) {
+    if (validatePhone(phone) === false) {
       inputPhone.parentNode.classList.add(`modal__error`);
       return false;
     } else {
@@ -108,13 +132,13 @@ const modalHandlers = () => {
   modalCloseButton.addEventListener(`click`, closeModal);
   modalMessageClose.addEventListener(`click`, closeModalMessage);
 
-  inputPhone.addEventListener(`input`, validatePhone);
-  inputEmail.addEventListener(`input`, validateEmail);
+  inputPhone.addEventListener(`input`, checkPhone);
+  inputEmail.addEventListener(`input`, checkEmail);
 
   modalForm.addEventListener(`submit`, (evt) => {
     evt.preventDefault();
-    const isValidPhone = validatePhone();
-    const isValidEmail = validateEmail();
+    const isValidPhone = checkPhone();
+    const isValidEmail = checkEmail();
 
     if (isValidEmail && isValidPhone) {
       closeModal();
@@ -123,5 +147,83 @@ const modalHandlers = () => {
   });
 };
 
+const formHandlers = () => {
+  const modalMessage = document.querySelector(`.modal--message`);
+  const modalMessageClose = modalMessage.querySelector(`.modal__close`);
+  const modalOverlay = document.querySelector(`.modal__overlay`);
+
+  const form = document.querySelector(`.feedback__form`);
+  const inputPhone = form.querySelector(`.feedback__input--phone`);
+  const inputEmail = form.querySelector(`.feedback__input--email`);
+
+  if (localStorage.getItem(`phone`)) {
+    inputPhone.value = localStorage.getItem(`phone`);
+  }
+
+  if (localStorage.getItem(`email`)) {
+    inputEmail.value = localStorage.getItem(`email`);
+  }
+
+  const checkEmail = () => {
+    const email = inputEmail.value;
+    localStorage.setItem(`email`, email);
+
+    if (validateEmail(email) === false) {
+      inputEmail.parentNode.classList.add(`feedback__error`);
+      return false;
+    } else {
+      inputEmail.parentNode.classList.remove(`feedback__error`);
+      return true;
+    }
+  };
+
+  const checkPhone = () => {
+    const phone = inputPhone.value;
+    localStorage.setItem(`phone`, phone);
+
+    if (validatePhone(phone) === false) {
+      inputPhone.parentNode.classList.add(`feedback__error`);
+      return false;
+    } else {
+      inputPhone.parentNode.classList.remove(`feedback__error`);
+      return true;
+    }
+  };
+
+  const onEscDown = (evt) => {
+    if (evt.code === `Escape` || evt.keyCode === 27) {
+      closeModalMessage();
+    }
+  };
+
+  const openModalMessage = () => {
+    modalMessage.style.display = `block`;
+    modalOverlay.style.display = `block`;
+    modalOverlay.addEventListener(`click`, closeModalMessage);
+    document.addEventListener(`keydown`, onEscDown);
+  };
+
+  const closeModalMessage = () => {
+    modalMessage.style.display = `none`;
+    modalOverlay.style.display = `none`;
+    modalOverlay.removeEventListener(`click`, closeModalMessage);
+    document.removeEventListener(`keydown`, onEscDown);
+  };
+
+  inputPhone.addEventListener(`input`, checkPhone);
+  inputEmail.addEventListener(`input`, checkEmail);
+
+  form.addEventListener(`submit`, (evt) => {
+    evt.preventDefault();
+    const isValidPhone = checkPhone();
+    const isValidEmail = checkEmail();
+
+    if (isValidEmail && isValidPhone) {
+      openModalMessage();
+    }
+  });
+};
+
 menu();
 modalHandlers();
+formHandlers();
